673237d0cfade0c2ed81b6f1371fca544888a03f
MATANBENSHABAT <matanbs2000@gmail.com>
2023-02-23 14:23:36 +0200

    mergesort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -32,8 +32,26 @@ function selectionSort(arr) {
   return arr;
 }
 
-function mergeSort(arr) {}
+function mergeSort(arr) {
+  if(arr.length === 1) return arr;
+  const center = Math.floor(arr.length/2);
+  const right =arr.slice(0,center)
+  const left =arr.slice(center)
+  
+  return merge(mergeSort(right),mergeSort(left))
+}
 
-function merge(left, right) {}
+function merge(left, right) {
+  const results = [];
+  while(left.length && right.length)
+  {
+    if(left[0]<right[0]){
+      results.push(left.shift())
+    }else{
+      results.push(right.shift())
+    }
+  }
+  return [...results,...left,...right]
+}
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
